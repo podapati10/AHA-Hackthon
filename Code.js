6e31579f1256640b50282c1ef13a5c013760e807
podapati10 <63558973+podapati10@users.noreply.github.com>
2026-02-04 19:44:22 -0700

    Update Code.js

--- Code.js
+++ Code.js
@@ -203,7 +203,7 @@ svgA_g.selectAll(".state").remove()
         {
             m;
         }
-var Curr_date = "2023-03-10";
+var Curr_date = "2023-03-10 04:31:51";
 var Temp_Var1=[], s_v1 =[], s_v2 =[], Var1 = {}, y_R, yAxis_R, colorLegend;
 
 for( let i=0; i<State_d.length; i++)
@@ -745,7 +745,7 @@ if(10 > m)
         {
             m;
         }
-var Curr_date = "2023-03-10";
+var Curr_date = "2023-03-10 04:31:51";
 console.log(Curr_date)
 var cnt=0, cnt1=0;
 for(let i=0; i<State_Dataset.length; i++)
@@ -1808,6 +1808,7 @@ countyInfo.remove();})
 //     if (playing)
 //         setTimeout(step(), 100)
 //  }
+
 
 
 
